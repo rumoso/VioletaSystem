d4dfd42578a658d86e2fdc85d808b373e61e8b2c
rumoso <ing.rubenmoreno@gmail.com>
2024-08-10 13:41:48 -0700

    Se realiza instalación de cambios
    Se agrega reporte de pagos y se agrega permiso para ver los costos en el reporte de inventarios

--- Front/src/environments/environment.ts
+++ Front/src/environments/environment.ts
@@ -1,20 +1,13 @@
 export const environment = {
     production: false,
 
-    //appName: 'Violeta Sistem',
-    //iconApp: 'assets/img/icons/VioletaIcon.png',
+    appName: 'Violeta Sistem',
+    iconApp: 'assets/img/icons/VioletaIcon.png',
 
-    //baseUrl: 'http://192.168.1.2:8082',// OFICINA GUAMUCHIL
+    baseUrl: 'http://192.168.1.2:8082',// OFICINA GUAMUCHIL
     //baseUrl: 'http://127.0.0.1:8082',
 
     appMain: 'VioletaSistem',
-
-
-    appName: 'Sarita Sistem',
-    iconApp: 'assets/img/icons/SaritaIcon.jpeg',
-
-    //baseUrl: 'http://192.168.1.2:8083',// OFICINA GUAMUCHIL
-    baseUrl: 'http://127.0.0.1:8082',
 
     idSucursal: 1
   };
