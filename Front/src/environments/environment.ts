26ffd7d1db51f96c8d4d0ca150c5228977c26e29
rumoso <ing.rubenmoreno@gmail.com>
2024-08-09 20:53:19 -0700

    Se versiona

--- Front/src/environments/environment.ts
+++ Front/src/environments/environment.ts
@@ -1,19 +1,19 @@
 export const environment = {
     production: false,
 
-    appName: 'Violeta Sistem',
-    iconApp: 'assets/img/icons/VioletaIcon.png',
+    //appName: 'Violeta Sistem',
+    //iconApp: 'assets/img/icons/VioletaIcon.png',
 
-    baseUrl: 'http://192.168.1.2:8082',// OFICINA GUAMUCHIL
+    //baseUrl: 'http://192.168.1.2:8082',// OFICINA GUAMUCHIL
     //baseUrl: 'http://127.0.0.1:8082',
 
     appMain: 'VioletaSistem',
 
 
-    // appName: 'Sarita Sistem',
-    // iconApp: 'assets/img/icons/SaritaIcon.jpeg',
+    appName: 'Sarita Sistem',
+    iconApp: 'assets/img/icons/SaritaIcon.jpeg',
 
-    // baseUrl: 'http://192.168.1.2:8083',// OFICINA GUAMUCHIL
+    baseUrl: 'http://192.168.1.2:8083',// OFICINA GUAMUCHIL
     //baseUrl: 'http://127.0.0.1:8082',
 
     idSucursal: 1
